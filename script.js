ce7fa2e392c5c9dab04f9548ad83405997c88350
Aleksandra Chyła <aleks.chyla@gmail.com>
2019-03-30 12:34:20 +0100

    Add method renderForm which render input and button.

--- script.js
+++ script.js
@@ -6,6 +6,20 @@ class TodoApp {
     }
 
     render(){
-
+        this.renderForm();
     }
+
+    renderForm() {
+        const div = document.createElement('div');
+        const input = document.createElement('input');
+        const button = document.createElement('button');
+
+        div.classList.add('form-container');
+        button.innerText = 'dodaj';
+
+        div.appendChild(input);
+        div.appendChild(button);
+        this.container.appendChild(div);
+    }
+
 }
