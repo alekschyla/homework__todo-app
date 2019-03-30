51b2f4470b780e855821c6e20f7e54e060e4a8c0
Aleksandra Chyła <aleks.chyla@gmail.com>
2019-03-30 12:11:55 +0100

    Create all needed files. Add do script.js app class.

---
+++ script.js
@@ -0,0 +1,11 @@
+class TodoApp {
+    constructor(selector) {
+        this.container = document.querySelector(selector) || document.body;
+        this.todos = [];
+        this.render();
+    }
+
+    render(){
+
+    }
+}
